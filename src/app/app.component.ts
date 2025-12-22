bfd7490b3c6c92bba3321aa3a3b0e400f800ef44
Harika Sana <harika.sana05@gmail.com>
2025-12-22 17:10:18 +0530

    portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { IntroComponent } from "./components/intro/intro.component";
+import { AboutComponent } from "./components/about/about.component";
+import { SkillsComponent } from "./components/skills/skills.component";
+import { ContactComponent } from "./components/contact/contact.component";
+import { ProjectsComponent } from "./components/projects/projects.component";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [IntroComponent, AboutComponent, SkillsComponent, ContactComponent, ProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
